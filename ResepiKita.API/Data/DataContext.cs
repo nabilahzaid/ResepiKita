970b811d00e261ea701b689b6f7e57d9411adce9
Md Zaid <mzalinks@gmail.com>
2020-04-15 01:42:56 +0800

    Initial Commit

---
+++ ResepiKita.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ResepiKita.API.Models;
+
+namespace ResepiKita.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ ResepiKita.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace ResepiKita.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
